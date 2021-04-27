39b9965c009df78bed98c8667271a4bdbb59fa57
Danilo Silva <danilo.silva@itenabler.com.br>
2021-04-27 10:05:38 -0300

    Cap 2

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 class Cliente{
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
-    saldo;
-    rg;
+
+    // https://github.com/tc39/proposal-class-fields#private-fields
+    // atributo privado padrão node coloca #nomeVar
+    // atributo privado padrão js coloca _nomeVar
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+            // console.log("\nSacado:",valor);
+            // console.log("Saldo:",this._saldo);
+        }
+    }
+
+    depositar(valor){
+        if(valor > 0){
+            this._saldo += valor;
+            // console.log("\nDepositado:",valor);
+            // console.log("Saldo:",this.#saldo);
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = "11122233309";
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg = 123456789;
-console.log(cliente1);
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = "88822233309";
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente2);
+const contaCorrenteRicardo = new ContaCorrente();
+// cria qualquer coisa o gruda ele nesse objeto
+// contaCorrenteRicardo.qualquerCoisa = 1000;
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.sacar(50);
+
+console.log(contaCorrenteRicardo);
