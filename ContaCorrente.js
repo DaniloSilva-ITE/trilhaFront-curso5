3c3cc8d851c128aff3972f94fc0546f046232c32
Danilo Silva <danilo.silva@itenabler.com.br>
2021-04-27 11:54:07 -0300

    Cap 4

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,11 +1,26 @@
+import {Cliente} from "./Cliente.js"
+
 export class ContaCorrente{
-    cliente;
+    _cliente;
     agencia;
 
     // https://github.com/tc39/proposal-class-fields#private-fields
     // atributo privado padrão node coloca #nomeVar
     // atributo privado padrão js coloca _nomeVar
     _saldo = 0;
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente)
+            this._cliente = novoValor;
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
 
     sacar(valor){
         if(this._saldo >= valor){
--- index.js
+++ index.js
@@ -1,31 +1,22 @@
-// https://docs.npmjs.com/cli/v7/configuring-npm/package-json
-// npm init 
-// enter all
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = "11122233309";
+const cc1 = new ContaCorrente();
+cc1.cliente = new Cliente();
+cc1.cliente.nome = "Ricardo";
+cc1.cliente.cpf = 11122233309;
+cc1.agencia = 1001;
+cc1.depositar(1000);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = "88822233309";
+const cc2 = new ContaCorrente();
+cc2.cliente = new Cliente();
+cc2.cliente.nome = "Alice";
+cc2.cliente.cpf = 88822233309;
+cc2.agencia = 1002;
+cc2.depositar(3000);
 
-const contaCorrenteRicardo = new ContaCorrente();
-// cria qualquer coisa o gruda ele nesse objeto
-// contaCorrenteRicardo.qualquerCoisa = 1000;
-contaCorrenteRicardo.cliente = cliente1;
-contaCorrenteRicardo.agencia = 1001;
+let valor = 200;
+cc1.transferir(valor, cc2);
 
-const contaCorrenteAlice = new ContaCorrente();
-contaCorrenteAlice.cliente = cliente2;
-contaCorrenteAlice.agencia = 1002;
-
-contaCorrenteRicardo.depositar(100);
-contaCorrenteAlice.depositar(100);
-
-contaCorrenteRicardo.transferir(50, contaCorrenteAlice);
-
-console.log(contaCorrenteRicardo);
-console.log(contaCorrenteAlice);
+console.log(cc1);
+console.log(cc2);
