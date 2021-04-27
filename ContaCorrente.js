2f5a81cee46a1066ccc0c7bd9b841cdca627aa7a
Danilo Silva <danilo.silva@itenabler.com.br>
2021-04-27 12:41:36 -0300

    Cap 5

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,17 +1,24 @@
 import {Cliente} from "./Cliente.js"
 
 export class ContaCorrente{
-    _cliente;
+    static numDeContas = 0;
     agencia;
 
     // https://github.com/tc39/proposal-class-fields#private-fields
     // atributo privado padrão node coloca #nomeVar
     // atributo privado padrão js coloca _nomeVar
+    _cliente;
     _saldo = 0;
 
     set cliente(novoValor){
         if(novoValor instanceof Cliente)
             this._cliente = novoValor;
+    }
+
+    constructor(cliente, agencia){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numDeContas++;
     }
 
     get cliente(){
--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cc1 = new ContaCorrente();
-cc1.cliente = new Cliente();
-cc1.cliente.nome = "Ricardo";
-cc1.cliente.cpf = 11122233309;
-cc1.agencia = 1001;
-cc1.depositar(1000);
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
 
-const cc2 = new ContaCorrente();
-cc2.cliente = new Cliente();
-cc2.cliente.nome = "Alice";
-cc2.cliente.cpf = 88822233309;
-cc2.agencia = 1002;
-cc2.depositar(3000);
+const cc1 = new ContaCorrente(cliente1, 1001);
+const cc2 = new ContaCorrente(cliente2, 1002);
+
+cc1.depositar(1000);
+cc2.depositar(1000);
 
 let valor = 200;
 cc1.transferir(valor, cc2);
 
 console.log(cc1);
-console.log(cc2);
+console.log(ContaCorrente.numDeContas);
+//console.log(cc2);
